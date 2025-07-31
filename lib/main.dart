cbfe12dad964bce2f1856098dc0a98559e88013f
Renish Hirani <142030406+RenishHirani@users.noreply.github.com>
2025-07-31 18:11:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:api/apiDemo.dart';
+import 'package:eval_22/least.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: apiDemo(),
+      home: Least(),
       debugShowCheckedModeBanner: false,
     );
   }
